bdc103c7d9a6e8ec6acb7a5b83025be56f263b29
Ceowyllian <downwell@online.ua>
2024-02-21 10:52:45 +0300

    feat(task-1): implement creation and multiplication of the matrices

--- task-1/task-1.cpp
+++ task-1/task-1.cpp
@@ -1,20 +1,105 @@
-﻿// task-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int** createMatrix(int n, int m) {
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++)
+        matrix[i] = new int[m];
+    return matrix;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void deleteMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++)
+        delete[] matrix[i];
+    delete[] matrix;
+}
+
+
+void fillMatrix(int** matrix, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("matrix[%d, %d] = ", i, j);
+            std::cin >> matrix[i][j];
+        }
+    }
+}
+
+
+bool isSymmetric(int** matrix, int n) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (matrix[i][j] != matrix[j][i])
+                return false;
+    return true;
+}
+
+
+int** createTransposedMatrix(int** sourceMatrix, int n, int m) {
+    int** targetMatrix = createMatrix(m, n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            targetMatrix[j][i] = sourceMatrix[i][j];
+    return targetMatrix;
+}
+
+
+int** multiply(int** leftMatrix, int** rightMatrix, int n, int p, int m) {
+    int** result = createMatrix(n, m);
+    for (int i = 0; i < n; i)
+        for (int j = 0; j < m; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < p; k++) {
+                result[i][j] += leftMatrix[i][k] * rightMatrix[k][j];
+            }
+        }
+    return result;
+}
+
+
+double calculateLength(int** g, int** x, int n) {
+    int** x_g = multiply(x, g, 1, n, n); // [1][n]
+    int** xt = createTransposedMatrix(x, 1, n); // [n][1]
+    int** x_g_xt = multiply(x_g, xt, 1, n, 1); // [1][1]
+
+    double length = std::sqrt(x_g_xt[0][0]);
+
+    deleteMatrix(x_g, 1);
+    deleteMatrix(xt, n);
+    deleteMatrix(x_g_xt, 1);
+
+    return length;
+}
+
+
+void readInputFile(const char* filePath, int& n, int**& g, int**& x) {
+    // TODO: enter n, g and x
+    g = createMatrix(n, n);
+    x = createMatrix(1, n);
+}
+
+
+int main(int argc, char* argv[])
+{
+    puts("Enter the path to the file: ");
+    char* filePath;
+    std::cin >> filePath;
+
+    int n;
+    int** g;
+    int** x;
+    readInputFile(filePath, n, g, x);
+
+    if (!isSymmetric(g, n)) {
+        puts("Error: matrix G is not symmetric.");
+        exit(-1);
+    }
+
+    int length = calculateLength(g, x, n);
+    printf("Vector X length: %d", length);
+
+    deleteMatrix(g, n);
+    deleteMatrix(x, 1);
+}
